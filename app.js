df577a3276be3cf51f14b03e5abfe842945bb3ee
Pedro Henrique Rossetto Natal <pedronatalr@gmail.com>
2024-06-06 16:34:50 -0300

    Conteudo estudado até o momento

---
+++ app.js
@@ -0,0 +1,35 @@
+/*var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+
+console.log("Hello world!");
+console.log("Meu nome é Pedro");
+console.log("Eu sou o better");
+
+if(mostrarSite){
+    console.log(site);
+}
+*/
+function soma(a,b){
+    return a + b;
+}
+function mult(a,b){
+    return a * b; 
+}
+function sub(a,b){
+    return a - b;
+}
+function div(a,b){
+    return a / b;
+}
+
+
+var resultado = mult(10,10);
+var resultadoSub = sub(10,10);
+
+
+
+console.log(div(10,2));
+console.log(soma(10,20));
+console.log(resultado);
+console.log(resultadoSub);
